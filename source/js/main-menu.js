5065f0fd731b81b952e938fb91b71158274d8335
Евгений Кривицкий <lombre.box@gmail.com>
2019-11-18 22:01:07 +0300

    backup

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -1,20 +1,40 @@
+var navVisible;
+var menu;
+var nav;
+
 const addMenuHandlers = function () {
 
-  const menu = document.body.querySelector(".header__menu");
-  const nav = document.body.querySelector(".header__nav");
+  menu = document.body.querySelector(".header__menu");
+  nav = document.body.querySelector(".header__nav");
 
   if (!menu || !nav) {
     return;
   }
+
+  navVisible = !nav.classList.contains("visually-hidden");
 
   menu.addEventListener('click', function () {
     const activeClass = "header__menu--active";
     if (menu.classList.contains(activeClass)) {
       menu.classList.remove(activeClass);
       nav.classList.add("visually-hidden");
+      navVisible = false;
     } else {
       menu.classList.add(activeClass);
       nav.classList.remove("visually-hidden");
+      navVisible = true;
+    }
+  });
+
+  window.addEventListener('resize', function () {
+    if (document.body.clientWidth >= 768) {
+      nav.classList.remove("visually-hidden");
+    } else {
+      if (navVisible) {
+        nav.classList.remove("visually-hidden");
+      } else {
+        nav.classList.add("visually-hidden");
+      }
     }
   });
 };
